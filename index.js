795951bb7b5e941faf73209ae2eaf0958f3115a9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-24 03:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Write your code in this file!
-function scuberGreetingForFeet(num)
-let msg
-if (num < 400) {
-  msg = 'This one is on me!'
+function scuberGreetingForFeet(num) {
+  let msg
+  if (num < 400) {
+    msg = 'This one is on me!'
+  }
+}
+
+function ternaryCheckCity() {
+  
+}
+
+
+function switchOnCharmFromTip() {
+  
 }
